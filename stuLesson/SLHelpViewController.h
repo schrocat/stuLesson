621ef5c5471ff080ffed946bbc923f1ac086cee2
薛汉鎱 <xuehanyuan@xuehanyuandeMac.local>
2017-12-10 10:18:53 +0800

    S

---
+++ stuLesson/SLHelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLHelpViewController.h
+//  stuLesson
+//
+//  Created by 薛汉鎱 on 2017/12/9.
+//  Copyright © 2017年 com.yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLHelpViewController : UIViewController
+
+@end
---
+++ stuLesson/SLHelpViewController.m
@@ -0,0 +1,37 @@
+//
+//  SLHelpViewController.m
+//  stuLesson
+//
+//  Created by 薛汉鎱 on 2017/12/9.
+//  Copyright © 2017年 com.yu. All rights reserved.
+//
+
+#import "SLHelpViewController.h"
+
+@interface SLHelpViewController ()
+
+@end
+
+@implementation SLHelpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
